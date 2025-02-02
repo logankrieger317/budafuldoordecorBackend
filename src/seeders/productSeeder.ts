e707b2c71f28acc3ee0c3375d7a3f0daa881d2d3
Logan <logankrieger317@gmail.com>
2025-02-02 13:38:25 -0600

    fix: replace null imageUrl values with empty strings to match type

--- src/seeders/productSeeder.ts
+++ src/seeders/productSeeder.ts
@@ -1,9 +1,8 @@
 import { Database } from '../models';
-import { ProductCreationAttributes } from '../types/models';
 
 const db = Database.getInstance();
 
-const productTemplate: ProductCreationAttributes[] = [
+const products = [
   {
     sku: 'LMK-020V56',
     name: 'silver lattice',
@@ -13,9 +12,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: '',
     price: 10,
     isWired: true,
-    quantity: 0,
     description: 'silver wired ribbon with lattice pattern and metallic trim/edge',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524201/LMK-020V56_tbdc47.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
@@ -27,23 +26,23 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: '',
     price: 9,
     isWired: true,
-    quantity: 0,
     description: 'white wired ribbon with stitched gold roses and trim',
     imageUrl: '',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
     sku: 'RN591502',
     name: 'black velvet',
-    width: 0.625, // 5/8"
+    width: 0.625,
     length: 10,
     color: 'black',
     brand: '',
     price: 11,
     isWired: false,
-    quantity: 0,
     description: 'black velvet unwired ribbon',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524207/RN591502_icwe6f.jpg',
+    quantity: 100,
     category: 'velvet-ribbon'
   },
   {
@@ -55,9 +54,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'RG',
     price: 11,
     isWired: true,
-    quantity: 0,
     description: 'white wired ribbon with gold stripes and polka dots',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524205/RF1510C6_mhsorh.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
@@ -69,9 +68,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'RG',
     price: 11,
     isWired: true,
-    quantity: 0,
     description: 'baby pink wired ribbon with hot pink and white swirls',
     imageUrl: '',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
@@ -83,23 +82,23 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: '',
     price: 9,
     isWired: true,
-    quantity: 0,
     description: 'baby pink embossed wired ribbon',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524201/JS25-903_q8ixig.jpg',
+    quantity: 100,
     category: 'embossed-ribbon'
   },
   {
     sku: 'RN591527',
     name: 'white velvet',
-    width: 0.625, // 5/8"
+    width: 0.625,
     length: 10,
     color: 'white',
     brand: '',
     price: 11,
     isWired: false,
-    quantity: 0,
     description: 'white velvet unwired ribbon',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524207/RN591527_a56qmw.jpg',
+    quantity: 100,
     category: 'velvet-ribbon'
   },
   {
@@ -111,9 +110,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'RG',
     price: 11,
     isWired: true,
-    quantity: 0,
     description: 'black wired ribbon with silver stripes and polka dots',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524206/RGF151087_lc9uso.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
@@ -125,13 +124,13 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: '',
     price: 9,
     isWired: true,
-    quantity: 0,
     description: 'red embossed wired ribbon',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524201/JS25--901_wkfyto.jpg',
+    quantity: 100,
     category: 'embossed-ribbon'
   },
   {
-    sku: 'RGF151085-1',
+    sku: 'RGF151085',
     name: 'silver/white stripe',
     width: 2.5,
     length: 10,
@@ -139,9 +138,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'RG',
     price: 11,
     isWired: true,
-    quantity: 0,
     description: 'white wired ribbon with silver stripes and polka dots',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524206/RGF151085_u19i6v.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
@@ -153,23 +152,23 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'RG',
     price: 11,
     isWired: true,
-    quantity: 0,
     description: 'hot pink wired ribbon with silver stripes and polka dots',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524206/RGF1510FT_hhtb3v.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
     sku: 'C024403',
     name: 'diamond dust #3',
-    width: 0.625, // 5/8"
+    width: 0.625,
     length: 100,
     color: 'gold',
     brand: 'Berwick',
     price: 20,
     isWired: false,
-    quantity: 0,
     description: 'gold diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
@@ -181,51 +180,51 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'Berwick',
     price: 24,
     isWired: false,
-    quantity: 0,
     description: 'gold diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
     sku: 'C024404',
     name: 'diamond dust #5',
-    width: 0.875, // 7/8"
+    width: 0.875,
     length: 100,
     color: 'gold',
     brand: 'Berwick',
     price: 22,
     isWired: false,
-    quantity: 0,
     description: 'gold diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
     sku: 'C024408',
     name: 'diamond dust #3',
-    width: 0.625, // 5/8"
+    width: 0.625,
     length: 100,
     color: 'silver',
     brand: 'Berwick',
     price: 20,
     isWired: false,
-    quantity: 0,
     description: 'silver diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
     sku: 'C024409',
     name: 'diamond dust #5',
-    width: 0.875, // 7/8"
+    width: 0.875,
     length: 100,
     color: 'silver',
     brand: 'Berwick',
     price: 22,
     isWired: false,
-    quantity: 0,
     description: 'silver diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
@@ -237,9 +236,9 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: 'Berwick',
     price: 24,
     isWired: false,
-    quantity: 0,
     description: 'silver diamond dust ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'diamond-dust-ribbon'
   },
   {
@@ -251,279 +250,282 @@ const productTemplate: ProductCreationAttributes[] = [
     brand: '',
     price: 10,
     isWired: true,
-    quantity: 0,
     description: 'gold wired ribbon with lattice pattern and metallic trim edge',
-    imageUrl: '',
+    imageUrl: 'https://res.cloudinary.com/dfszr2lob/image/upload/v1738524201/LMK-001A23_cxvtc7.jpg',
+    quantity: 100,
     category: 'wired-ribbon'
   },
   {
     sku: 'LMK-002B34',
     name: 'white #16',
-    width: 2, // 2"
+    width: 2,
     length: 100,
     color: 'white',
     brand: 'McGinley',
     price: 16,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label white ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
-  },
+  }
+];
+
+const mcGinleyProducts = [
   {
     sku: 'LMK-003C45',
     name: 'black #9',
-    width: 1.3125, // 1 5/16"
+    width: 1.3125,
     length: 100,
     color: 'black',
     brand: 'McGinley',
     price: 14,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label black ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-004D56',
     name: 'white #9',
-    width: 1.3125, // 1 5/16"
+    width: 1.3125,
     length: 100,
     color: 'white',
     brand: 'McGinley',
     price: 14,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label white ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-005E67',
     name: 'royal blue #5',
-    width: 0.875, // 7/8"
+    width: 0.875,
     length: 100,
     color: 'royal blue',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label royal blue ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-006F78',
     name: 'holiday gold #5',
-    width: 0.875, // 7/8"
+    width: 0.875,
     length: 100,
     color: 'gold',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label holiday gold ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-007G89',
     name: 'silver gray #5',
-    width: 0.875, // 7/8"
+    width: 0.875,
     length: 100,
     color: 'gray',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label silver gray ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-008H90',
     name: 'holiday gold #9',
-    width: 1.3125, // 1 5/16"
+    width: 1.3125,
     length: 100,
     color: 'gold',
     brand: 'McGinley',
     price: 14,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label holiday gold ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-009J12',
     name: 'white #1.5',
-    width: 0.3125, // 5/16"
+    width: 0.3125,
     length: 100,
     color: 'white',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'silver label acetate white ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'acetate-ribbon'
   },
   {
     sku: 'LMK-010K23',
     name: 'black #1.5',
-    width: 0.3125, // 5/16"
+    width: 0.3125,
     length: 100,
     color: 'black',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'silver label acetate black ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'acetate-ribbon'
   },
   {
     sku: 'LMK-011L34',
     name: 'royal blue #1.5',
-    width: 0.3125, // 5/16"
+    width: 0.3125,
     length: 100,
     color: 'royal blue',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'silver label acetate royal blue ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'acetate-ribbon'
   },
   {
     sku: 'LMK-012M45',
     name: 'holiday red #1.5',
-    width: 0.3125, // 5/16"
+    width: 0.3125,
     length: 100,
     color: 'red',
     brand: 'McGinley',
     price: 10,
     isWired: false,
-    quantity: 0,
     description: 'silver label acetate holiday red ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'acetate-ribbon'
   },
   {
     sku: 'LMK-013N56',
     name: 'black #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'black',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label black ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-014P67',
     name: 'holiday gold #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'gold',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label holiday gold ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-015Q78',
     name: 'holiday red #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'red',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label holiday red ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-016R89',
     name: 'silver gray #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'gray',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label silver gray ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-017S90',
     name: 'white #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'white',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label white ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-018T12',
     name: 'columbia blue #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'blue',
     brand: 'McGinley',
     price: 8,
     isWired: false,
-    quantity: 0,
     description: 'satin lustre gold label columbia blue ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'satin-ribbon'
   },
   {
     sku: 'LMK-019U34',
     name: 'pink #3',
-    width: 0.5625, // 9/16"
+    width: 0.5625,
     length: 100,
     color: 'pink',
     brand: 'McGinley',
     price: 12,
     isWired: false,
-    quantity: 0,
     description: 'silver label acetate pink ribbon',
     imageUrl: '',
+    quantity: 100,
     category: 'acetate-ribbon'
   }
 ];
 
-export async function seedProducts() {
+export const seedProducts = async () => {
   try {
-    console.log('Starting product seeding...');
+    // Clear existing products
+    await db.Product.destroy({ where: {} });
     
-    // Create all products
-    const products = await db.Product.bulkCreate(productTemplate);
+    // Insert all products
+    await db.Product.bulkCreate([...products, ...mcGinleyProducts]);
     
-    console.log(`Successfully seeded ${products.length} products`);
-    return products;
+    console.log('Products seeded successfully');
   } catch (error) {
     console.error('Error seeding products:', error);
     throw error;
   }
-}
+};
 
 // Run seeder if this file is run directly
 if (require.main === module) {
